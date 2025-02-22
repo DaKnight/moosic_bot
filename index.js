995340e0283fc8fa48fdb575422b7808e1796b82
DaKnight <dhruvkandpal02@gmail.com>
2025-02-22 20:04:58 +0530

    first successfull testing!

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+require('dotenv').config();
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+// const { token } = require('./config.json');
+require('dotenv');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on('messageCreate', message => {
+	if(message.author.bot) return;
+	message.reply({
+		content: 'Hi from Deepak bot!',
+	});
+});
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.login(token);
+client.login(process.env.TOKEN);
