598742905428f3b764b74a3667316a5e113b0a5d
appyouz <dikpalkhatri7@gmail.com>
2025-02-24 19:46:05 +0545

    Implemented eventHandling under events/ and cooldown

--- commands/utility/add.js
+++ commands/utility/add.js
@@ -1,8 +1,8 @@
 const {SlashCommandBuilder} = require('discord.js');
-const wait = require('node:timers/promises').setTimeout;
 
 
 module.exports = {
+  cooldown:5,
     data: new SlashCommandBuilder()
             .setName("add")
             .setDescription("Adding the song to playlist BOYS!!")
@@ -13,7 +13,6 @@ module.exports = {
             ),
             async execute(interaction) {
                 await interaction.deferReply();
-                await wait(2000);
                 const query = interaction.options.getString("query"); 
                 await interaction.editReply(`Adding ${query} to the playlist BOYS!!`);
             },
